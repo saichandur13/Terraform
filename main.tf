6a469ec55573485d52dda6839e492e4c463d55e2
saichandur13 <168154744+saichandur13@users.noreply.github.com>
2024-06-14 23:27:07 +0530

    Update main.tf
    Azure Provider: Authenticating using a Service Principal with a Client Secret

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ terraform {
 
 # block of the code skip_provider_registration = true This is only required when the User,\
 #Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+#Azure Provider: Authenticating using a Service Principal with a Client Secret
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
